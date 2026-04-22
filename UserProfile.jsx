12adc50529a6ea48b550ce37f541ccdd9bed7cd6
mohanapri1612-wq <mohanapri1612@gmail.com>
2026-04-22 10:28:15 +0530

    Create UserProfile.jsx

---
+++ UserProfile.jsx
@@ -0,0 +1,11 @@
+const UserProfile =() =>{
+    return(
+        <div style={{ display:"flex",
+            gap:"2rem", border: "1px solid blue",
+            backgroundColor: 'lightgray', width:"300px",
+            height:"300px"}} >
+          UserProfile
+        </div>
+    );
+};
+export default UserProfile;
