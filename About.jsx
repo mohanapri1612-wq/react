840e7eaca6de5d9bd18743d140133b84497b469d
mohanapri1612-wq <mohanapri1612@gmail.com>
2026-04-22 10:24:43 +0530

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,12 @@
+const About =() =>{
+    return(
+        <div style={{ display:"flex",
+            gap:"2rem", border: "1px solid green",
+            backgroundColor: 'pink', width:"300px",
+            height:"300px"}} >
+          About
+        </div>
+    );
+
+};
+export default About;
