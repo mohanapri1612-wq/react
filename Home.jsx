c0fe7db6cfed2fb3d7e917a6663b63753ec91471
mohanapri1612-wq <mohanapri1612@gmail.com>
2026-04-22 10:26:33 +0530

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+const Home =() =>{
+    return(
+        <div style={{ display:"flex",
+            gap:"2rem", border: "1px solid blue",
+            backgroundColor: 'lightblue', width:"300px",
+            height:"300px"}} >
+          Home
+        </div>
+    );
+
+};
+export default Home;
