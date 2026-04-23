afc03e0126a97463b65cbe16176bca705479e15c
mohanapri1612-wq <mohanapri1612@gmail.com>
2026-04-23 17:20:44 +0530

    Create TrainerDetails.jsx

---
+++ TrainerDetails.jsx
@@ -0,0 +1,10 @@
+function TrainerDetails() {
+  return (
+    <div>
+      <h2>Trainer Details</h2>
+      <p>Trainer ID:<input type="text" placeholder="Enter Trainer ID" /></p>
+    </div>
+  );
+}
+
+export default TrainerDetails;
